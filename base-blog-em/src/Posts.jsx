0a7958bff0c865ae08370c504dbfb2977386c87a
KOUTSODIMOS Dimitris <dimitris.koutsodimos@netcompany.com>
2025-03-19 11:56:59 +0200

    Handle Loading and Error States

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -10,12 +10,20 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const { data } = useQuery({
+  const { data, isLoading, isError, error } = useQuery({
     queryKey: ['posts'],
     queryFn: fetchPosts,
   });
 
-  if (!data) return <div />;
+  if (isLoading) return <h3>Loading</h3>;
+
+  if (isError)
+    return (
+      <>
+        <h3>Ooops, something went wrong!</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
 
   return (
     <>
